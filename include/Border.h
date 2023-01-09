868761dfe291f3c832be86a9f9831b1f05892946
Richard Buckley <richard.buckley@outlook.com>
2023-01-09 11:28:16 -0500

    Add Border class.

---
+++ include/Border.h
@@ -0,0 +1,30 @@
+//
+// Created by richard on 09/01/23.
+//
+
+/*
+ * Border.h Created by Richard Buckley (C) 09/01/23
+ */
+
+/**
+ * @file Border.h
+ * @author Richard Buckley <richard.buckley@ieee.org>
+ * @version 1.0
+ * @date 09/01/23
+ * @brief 
+ * @details
+ */
+
+#ifndef ROSE2_BORDER_H
+#define ROSE2_BORDER_H
+
+
+/**
+ * @class Border
+ */
+class Border {
+
+};
+
+
+#endif //ROSE2_BORDER_H
---
+++ src/Border.cpp
@@ -0,0 +1,16 @@
+//
+// Created by richard on 09/01/23.
+//
+
+/*
+ * Border.cpp Created by Richard Buckley (C) 09/01/23
+ */
+
+/**
+ * @file Border.cpp
+ * @author Richard Buckley <richard.buckley@ieee.org>
+ * @version 1.0
+ * @date 09/01/23
+ */
+
+#include "Border.h"
